fc6c87e66bd60ad7d4a719bfad6e3d0af20d5d62
tamaUdon <sakurayuki603@gmail.com>
2023-03-03 00:25:16 +0900

    add script to connect with websocket

---
+++ tempwriter/views/websocket.js
@@ -0,0 +1,46 @@
+// ref. https://qiita.com/shibaura/items/48bca63ac992c2f93b9d
+
+if (!window.Escpos) {
+    window.Escpos = {};
+}
+
+var webSocket; //ウェブソケット
+var messageTextArea = document.getElementById("messageTextArea"); // HTML内のテキスト出力エリア
+
+// サーバとの通信を接続する関数
+function connect(){
+    webSocket = new WebSocket("ws://localhost:8001"); // インスタンスを作り、サーバと接続
+
+    // ソケット接続すれば呼び出す関数を設定
+    webSocket.onopen = function(message){
+    messageTextArea.value += "Server connect... OK\n";
+    };
+
+    // ソケット接続が切ると呼び出す関数を設定
+    webSocket.onclose = function(message){
+    messageTextArea.value += "Server Disconnect... OK\n";
+    };
+
+    // ソケット通信中でエラーが発生すれば呼び出す関数を設定
+    webSocket.onerror = function(message){
+    messageTextArea.value += "error...\n";
+    };
+
+    // ソケットサーバからメッセージが受信すれば呼び出す関数を設定
+    webSocket.onmessage = function(message){
+    messageTextArea.value += "Receive => "+message.data+"\n";
+    };
+}
+
+// サーバにメッセージを送信する関数
+function sendMessage(){
+    var message = document.getElementById("textMessage");
+    messageTextArea.value += "Send => "+message.value+"\n";
+    webSocket.send(message.value);
+    message.value = "";
+}
+
+// サーバとの通信を切断する関数
+function disconnect(){
+    webSocket.close();
+}

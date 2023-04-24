3dd238f092ab93ab735c0095aa8df3eada8f7310
tamaUdon <sakurayuki603@gmail.com>
2023-04-25 00:23:21 +0900

    wip: traslate js to py

---
+++ printer/controller.py
@@ -0,0 +1,62 @@
+import usb.core
+import usb.util
+
+async def init():
+    # pyusbのチュートリアルから
+    # https://github.com/pyusb/pyusb/blob/master/docs/tutorial.rst
+    # find our device
+    dev = usb.core.find(idVendor=0xfffe, idProduct=0x0001)
+
+    # was it found?
+    if dev is None:
+        raise ValueError('Device not found')
+
+    # set the active configuration. With no arguments, the first
+    # configuration will be the active one
+    dev.set_configuration()
+
+    # get an endpoint instance
+    cfg = dev.get_active_configuration()
+    intf = cfg[(0,0)]
+
+    ep = usb.util.find_descriptor(
+        intf,
+        # match the first OUT endpoint
+        custom_match = \
+        lambda e: \
+            usb.util.endpoint_direction(e.bEndpointAddress) == \
+            usb.util.ENDPOINT_OUT)
+
+    assert ep is not None
+
+    # write the data
+    # 1. testする
+    ep.write('test')
+
+    # 2. printerをpythonで書き直す
+    # printer = new Escpos.Usb(devices[0]);
+    # await printer.setup();
+    # showMessage(`Connected to ${devices[0].productName} (${devices[0].serialNumber})`);
+
+    # 3. jsのpair()を書き直す
+
+### --js-- ### 
+# async function init () {
+#     const devices = await navigator.usb.getDevices();
+#     if (devices.length < 1) {
+#         showMessage('No paired device found. Click "pair" to pair a new printer.');
+#         return;
+#     }
+#     printer = new Escpos.Usb(devices[0]);
+#     await printer.setup();
+#     showMessage(`Connected to ${devices[0].productName} (${devices[0].serialNumber})`);
+# }
+
+# async function pair () {
+#     const device = await navigator.usb.requestDevice({filters: []});
+#     printer = new Escpos.Usb(device);
+#     await printer.setup();
+#     showMessage(`Connected to ${device.productName} (${device.serialNumber})`);
+# }
+
+init()

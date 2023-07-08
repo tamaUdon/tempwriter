6c039a998f036c559cb2d0d8f32375182ab27fb0
tamaUdon <sakurayuki603@gmail.com>
2023-07-09 02:39:33 +0900

    connect printer

--- printer/controller.py
+++ printer/controller.py
@@ -10,17 +10,29 @@ p = None
 def init_printer():
     global p
 
-    p = Usb(idVendor=0x04b8, idProduct=0x0202)
+    p = Usb(0x04b8, 0x0202, 0, profile="TM-T88III")
     # test print
-    text = "Lorem ipsum dolor （中略） id est laborum."
+    text = "初期化しました"
     p.text(text)
     p.cut()
 
-def output_and_cut(txt):
+def write(txt):
     global p
-
     try:
         p.text(txt)
+    except Exception as e:
+        print(e)
+
+def image(path):
+    global p
+    try:
+        p.image(path)
+    except Exception as e:
+        print(e)
+
+def cut():
+    global p
+    try:
         p.cut()
     except Exception as e:
         print(e)
